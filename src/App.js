fe30808c64d5260042c3c1b1e734d71067265a0d
GunPKate <gunPK>
2024-02-16 22:18:23 +0700

    initial components and basic css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Home from './components/home/Home';
+import Portfolio from './components/portfolio/Portfolio';
+import Resume from './components/resume/Resume';
+import Service from './components/servcie/Service';
+import Sidebar from './components/sidebar/Sidebar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar/>
+      <main className="main">
+        <About/>
+        <Contact/>
+        <Home/>
+        <Portfolio/>
+        <Resume/>
+        <Service/>
+      </main>
+    </>
   );
 }
 
