31ca5291f5bd2427df9e3434bc61f337ee50a883
GunPKate <gunPK>
2024-02-16 23:03:53 +0700

    additional css and pics

---
+++ src/components/work/data.jsx
@@ -0,0 +1,32 @@
+import Work1 from "../../../src/assets/work1.jpg"
+import Work2 from "../../../src/assets/work2.jpg"
+import Work3 from "../../../src/assets/work3.jpg"
+import Work4 from "../../../src/assets/work4.jpg"
+import Work5 from "../../../src/assets/work5.jpg"
+
+export const projectData = [
+    {
+        id: 1,
+        image: Work1,
+        title: "Web Design",
+        category: "web"
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "Web Design",
+        category: "web"
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "Web Design",
+        category: "web"
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "Web Design",
+        category: "web"
+    },
+]
